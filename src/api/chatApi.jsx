0b6906781d495fbd6c73925c4a76c4e2db3d87b8
nataliakif <nataliakif@gmail.com>
2025-05-07 13:57:25 +0200

    Update chatApi.jsx

--- src/api/chatApi.jsx
+++ src/api/chatApi.jsx
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5001/api',
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    'https://chat-backend-g81a.onrender.com/api',
   timeout: 5000,
   withCredentials: true,
 });
